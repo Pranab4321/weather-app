c637b30eb9898c98b5f144676f49786a71513923
Pranab <mahanandapranab@gmail.com>
2025-02-12 08:12:53 +0530

    Implemented function to fetch current weather data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-document.getElementById('search-btn').addEventListener('click',
-    function() {
-     const city = document.getElementById('city-input').value;
-     console.log('City entered:', city);
-     // Additional code to fetch weather data will be added later
-    });
+document.getElementById("search-btn").addEventListener("click", function () {
+  const city = document.getElementById("city-input").value;
+  console.log("City entered:", city);
+
+  const apiKey = "443ab80610e13ff973f3a07e97125163";
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}`;
+
+  async function fetchWeather() {
+    const response = await fetch(apiURL + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  fetchWeather();
+})
